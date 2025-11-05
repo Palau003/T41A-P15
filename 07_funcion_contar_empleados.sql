efc88f506149ccee1c0a3d87187640663ad56635
Palau003 <178561@upslp.edu.mx>
2025-11-04 19:23:56 -0600

    Create 07_funcion_contar_empleados.sql

---
+++ 07_funcion_contar_empleados.sql
@@ -0,0 +1,13 @@
+-- Función que cuenta cuántos empleados hay en un departamento
+
+CREATE OR REPLACE FUNCTION contar_empleados(dep_id INT)
+RETURNS INT AS $$
+DECLARE
+    total INT;
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM empleados
+    WHERE departamento_id = dep_id;
+    RETURN total;
+END;
+$$ LANGUAGE plpgsql;
