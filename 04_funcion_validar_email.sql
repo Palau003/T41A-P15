2465af612fd907a511876ff0b2dd4a6bdeeb2404
Palau003 <178561@upslp.edu.mx>
2025-11-04 19:23:09 -0600

    Create 04_funcion_validar_email.sql

---
+++ 04_funcion_validar_email.sql
@@ -0,0 +1,8 @@
+-- Función para validar si un correo contiene '@'
+
+CREATE OR REPLACE FUNCTION validar_email(correo TEXT)
+RETURNS BOOLEAN AS $$
+BEGIN
+    RETURN POSITION('@' IN correo) > 0;
+END;
+$$ LANGUAGE plpgsql;
