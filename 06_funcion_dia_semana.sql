a8a50694d833c09248dddf864c9b02add5823eb3
Palau003 <178561@upslp.edu.mx>
2025-11-07 13:37:36 -0600

    Delete 06_funcion_dia_semana.sql

--- 06_funcion_dia_semana.sql
+++
@@ -1,8 +0,0 @@
--- Función que devuelve el día de la semana a partir de una fecha
-
-CREATE OR REPLACE FUNCTION obtener_dia_semana(fecha DATE)
-RETURNS TEXT AS $$
-BEGIN
-    RETURN TO_CHAR(fecha, 'Day');
-END;
-$$ LANGUAGE plpgsql;
